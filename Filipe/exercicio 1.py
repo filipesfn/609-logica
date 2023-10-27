b81fc95e12febbe75f50ac0d805beb4b25fd8d85
filipesfn <148567553+filipesfn@users.noreply.github.com>
2023-10-27 12:34:01 -0300

    Add files via upload

---
+++ Filipe/exercicio 1.py
@@ -0,0 +1,20 @@
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+if numero1 > numero2:
+    print(f"O {numero1} é o maior")
+elif numero1 < numero2:
+    print(f"O {numero2} é o maior")
+else :
+    print("Os números são iguais")
+
+
+
+
+
+
+
+
+
+
+    
---
+++ Filipe/exercicio 2.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um número"))
+
+if numero > 0:
+    print(f"Esse {número} é positivo")
+elif numero < 0:
+    print(f"Esse {número} é negativo")
+else:
+    print (f"Esses número é neutro")
+
