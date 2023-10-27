de6a5929d9faf7f0d1cb69e6a73d6ef77c44caf8
filipesfn <148567553+filipesfn@users.noreply.github.com>
2023-10-27 12:37:00 -0300

    Delete Filipe directory

--- Filipe/exercicio 2.py
+++
@@ -1,9 +0,0 @@
-numero = int(input("Digite um número"))
-
-if numero > 0:
-    print(f"Esse {número} é positivo")
-elif numero < 0:
-    print(f"Esse {número} é negativo")
-else:
-    print (f"Esses número é neutro")
-
